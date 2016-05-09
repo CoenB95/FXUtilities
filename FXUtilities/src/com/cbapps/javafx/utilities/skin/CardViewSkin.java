753379cd58481b7d29f359c4b93dba83c764663b
CoenB95 <boelhouwers.coen@gmail.com>
2016-05-09 11:18:34 +0200

    CardView's style is now managed in the CSS. Focus indication added.

--- FXUtilities/src/com/cbapps/javafx/utilities/skin/CardViewSkin.java
+++ FXUtilities/src/com/cbapps/javafx/utilities/skin/CardViewSkin.java
@@ -2,29 +2,20 @@ package com.cbapps.javafx.utilities.skin;
 
 import com.cbapps.javafx.utilities.control.CardView;
 
-import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.control.SkinBase;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 public class CardViewSkin extends SkinBase<CardView> {
 	
 	public CardViewSkin(CardView control) {
 		super(control);
 		VBox box = new VBox();
-		getSkinnable().backgroundProperty().bind(Bindings.when(box.hoverProperty())
-				.then(new Background(new BackgroundFill(Color.gray(0.9),
-				new CornerRadii(2),null)))
-				.otherwise(new Background(new BackgroundFill(Color.WHITE,
-						new CornerRadii(2),null))));
-		getSkinnable().setEffect(new DropShadow());
+		getSkinnable().setId("cardview");
+		getSkinnable().getStylesheets().add("/com/cbapps/javafx/"
+				+ "utilities/styles/CleanTheme.css");
 		getSkinnable().onMouseClickedProperty().bind(control.onClickedProperty());
 		box.setPadding(new Insets(5));
 		Label titleLabel = new Label();
