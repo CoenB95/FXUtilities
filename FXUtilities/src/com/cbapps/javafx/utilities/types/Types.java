77708bf7754ba33b5a7cc114b5a4ed9781c45a46
CoenB95 <boelhouwers.coen@gmail.com>
2016-05-18 12:12:59 +0200

    Allow single bytes/integers.

--- FXUtilities/src/com/cbapps/javafx/utilities/types/Types.java
+++ FXUtilities/src/com/cbapps/javafx/utilities/types/Types.java
@@ -7,7 +7,7 @@ public class Types {
 	 * @param inputBuffer the input to be converted.
 	 * @return the converted result as a integer array.
 	 */
-	public static int[] toUnsignedBytes(byte[] inputBuffer) {
+	public static int[] toUnsignedBytes(byte... inputBuffer) {
         int[] outputBuffer = new int[inputBuffer.length];
         for(int i = 0; i < inputBuffer.length; i++){
             if(inputBuffer[i] < 0){
@@ -25,7 +25,7 @@ public class Types {
 	 * @param inputBuffer the input to be converted.
 	 * @return the converted result in a byte array.
 	 */
-	public static byte[] toSignedBytes(int[] inputBuffer) {
+	public static byte[] toSignedBytes(int... inputBuffer) {
 		byte[] outputBuffer = new byte[inputBuffer.length];
 		for(int i = 0; i < inputBuffer.length; i++){
             outputBuffer[i] = (byte)inputBuffer[i];
